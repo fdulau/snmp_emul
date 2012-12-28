c38aeef8e520b2317ef2738788ba338ddd8a4239
DULAUNOY Fabrice <fabrice@dulaunoy.be>
2012-12-28 11:56:11 +0100

    put a correct path (/opt/snmp_emul/... )

--- snmp_emul_iwatch.sh
+++ snmp_emul_iwatch.sh
@@ -4,11 +4,11 @@
 FROM=$1
 FILE_FULL=$2
 FILE=$( basename $FILE_FULL )
-BASE="/home/GIT/snmp_emul/conf/available"
+BASE="/opt/snmp_emul/conf/available"
 REAL_FILE="$BASE/$FILE"
 EVENT=$3
 
-CMD='/home/GIT/snmp_emul/parse_snmp_emul.pl'
+CMD='/opt/snmp_emul/bin/parse_snmp_emul.pl'
 
 case $EVENT in
   "IN_DELETE") $( $CMD -f $REAL_FILE -D );;  
